42483c35a49a95cbb066bd93f28b515c8660cd3e
Justin Wilkinson <wilkinsense@gmail.com>
2016-03-07 11:44:46 -0500

    Added scripts.

---
+++ Assets/Alarm.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class Alarm : MonoBehaviour {
+
+  public AudioSource alarmSound;
+  public GameObject ball;
+
+  // Use this for initialization
+  void Start() {
+
+  }
+
+  void OnTriggerEnter(Collider collider) {
+    if(collider.gameObject == ball) {
+      alarmSound.Play();
+    }
+  }
+
+  void OnTriggerExit(Collider collider) {
+    if(collider.gameObject == ball) {
+      alarmSound.Stop();
+    }
+  }
+
+}
---
+++ Assets/Goal.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+  public AudioSource goalSound;
+  public GameObject ball;
+
+  public void OnCollisionEnter(Collision collision) {
+    // If the ball has touched the goal, then we want to play a sound.
+    if(collision.collider.gameObject == ball) {
+      goalSound.Play();
+    }
+  }
+}
